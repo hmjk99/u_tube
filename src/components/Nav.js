751b80c4d5dc0fdeb02ffa09800b6ba04bfdc7c9
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-05-13 23:37:36 -0400

    creating nav

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { logo } from '../utils/constants'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        <img id='logo' src={logo} alt='logo'/>
+    </div>
+  )
+}
+
+export default Nav

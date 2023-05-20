a9fef3b520fbb8dddb6add9044236d3661098d94
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-05-20 17:50:37 -0400

    working on retrieving data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Nav from './components/Nav';
-import Feed from './components/Feed';
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
-function App() {
-  return (
-    <>
-    <Nav/>
-    <BrowserRouter>
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Feed/>}/>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
-    </BrowserRouter>
-    </>
-  );
-}
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
